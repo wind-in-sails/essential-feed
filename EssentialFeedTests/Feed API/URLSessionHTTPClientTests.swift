6783e1289c1463c8bc162cc4a95fb08b855357e1
Sergey Kudryavtsev <sergey.developer@bk.ru>
2026-09-03 11:13:04 -0700

    Creates URLSession data task with URL on creation and resumes data task on creation.

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -17,15 +17,14 @@ class URLSessionHTTPClient {
     }
     
     func get(from url: URL) {
-        session.dataTask(with: url) { _, _, _ in
-            //
-        }
+        let task = session.dataTask(with: url) { _, _, _ in }
+        task.resume()
     }
 }
 
 final class URLSessionHTTPClientTests: XCTestCase {
 
-    func test() {
+    func test_getFromURL_createsDataTaskWithURL() {
         let url = URL(string: "https://a-url.com")!
         let session = URLSessionSpy()
         let sut = URLSessionHTTPClient(session: session)
@@ -34,15 +33,38 @@ final class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertEqual(session.reseivedURLs, [url])
     }
 
+    func test_getFromURL_resumesDataTaskWithURL() {
+        let url = URL(string: "https://a-url.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stub(url: url, task: task)
+        let sut = URLSessionHTTPClient(session: session)
+        sut.get(from: url)
+
+        XCTAssertEqual(task.resumesCallCount, 1)
+    }
+
     // MARK: -Helpers
     private class URLSessionSpy: URLSession, @unchecked Sendable {
         var reseivedURLs: [URL] = []
+        var stubs: [URL: URLSessionDataTaskSpy] = [:]
 
+        func stub(url: URL, task: URLSessionDataTaskSpy) {
+            stubs[url] = task
+        }
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             reseivedURLs.append(url)
-            return FakeURLSessionDataTask()
+            return stubs[url] ?? FakeURLSessionDataTask()
         }
     }
 
-    private class FakeURLSessionDataTask: URLSessionDataTask, @unchecked Sendable { }
+    private class FakeURLSessionDataTask: URLSessionDataTask, @unchecked Sendable {
+        override func resume() {}
+    }
+    private class URLSessionDataTaskSpy: URLSessionDataTask, @unchecked Sendable {
+        var resumesCallCount = 0
+        override func resume() {
+            resumesCallCount += 1
+        }
+    }
 }
