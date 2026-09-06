ee46f41d99cba6036c0c53c29275aac27ac211b9
Sergey Kudryavtsev <sergey.developer@bk.ru>
2026-09-06 10:01:09 -0700

    Load command does not deliver a load result after the instance has been deallocated to prevent unexpected behaviors in clients' code

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -31,7 +31,8 @@ public final class LocalFeedLoader {
     }
 
     public func load(completion: @escaping (LoadResult) -> Void) {
-        store.retrieve { [unowned self] result in
+        store.retrieve { [weak self] result in
+            guard let self else { return }
             switch result {
             case let .found(feed, timestamp) where validate(timestamp):
                 completion(.success(feed.toModels()))
