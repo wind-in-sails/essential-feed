150c1d93cc23b90a07c8e434069ee914c7b11c85
Sergey Kudryavtsev <sergey.developer@bk.ru>
2026-09-06 10:37:34 -0700

    Extract duplicate test helpers into a shared scope

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -25,7 +25,7 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
     func test_load_failsOnRtrivalError() {
         let (sut, store) = makeSUT()
         let retrievalError = anyNSError()
-        
+
         expect(sut, toCompletteWithError: .failure(retrievalError)) {
             store.completeRetrieval(with: retrievalError)
         }
@@ -165,33 +165,5 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
         }
         action()
         wait(for: [exp], timeout: 1.0)
-    }
-
-    private func anyNSError() -> NSError {
-        return NSError(domain: "any error", code: 1)
-    }
-
-    private func uniqueImage() -> FeedImage {
-        return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
-    }
-
-    private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
-        let models = [uniqueImage(), uniqueImage()]
-        let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
-        return (models, local)
-    }
-
-    private func anyURL() -> URL {
-        return URL(string: "http://any-url.com")!
-    }
-}
-
-private extension Date {
-    func adding(days: Int) -> Date {
-        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
-    }
-
-    func adding(seconds: TimeInterval) -> Date {
-        return self + seconds
     }
 }
