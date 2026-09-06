16c5b3ff0fd3ad49a67ec08ef5f336c42ac089c9
Sergey Kudryavtsev <sergey.developer@bk.ru>
2026-09-06 09:39:30 -0700

    Load command deletes cache on retrieval error

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -38,6 +38,7 @@ public final class LocalFeedLoader {
             case .found, .empty:
                 completion(.success([]))
             case let .failure(error):
+                self.store.deleteCacheFeed(completion: { _ in })
                 completion(.failure(error))
             }
         }
