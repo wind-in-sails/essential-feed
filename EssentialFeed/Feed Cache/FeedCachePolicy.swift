15b72cf44d29bdd332cff757777c81e187e7965a
Sergey Kudryavtsev <sergey.developer@bk.ru>
2026-09-13 19:31:23 -0700

    remove internal access modifier since it is not nessessary

--- EssentialFeed/Feed Cache/FeedCachePolicy.swift
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-internal enum FeedCachePolicy {
+enum FeedCachePolicy {
     private static let calendar = Calendar(identifier: .gregorian)
 
     private static let maxCacheAgeInDays = 7
 
-    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
         guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
             return false
         }
