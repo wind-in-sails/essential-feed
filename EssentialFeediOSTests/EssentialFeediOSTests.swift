77d7a5dfb00c21935c7f767f3a12a23fb8e6b4a3
Sergey Kudryavtsev <sergey.developer@bk.ru>
2026-09-19 23:50:28 -0700

    Add `EssentialFeediOS` framework (prod and test) targets for iOS platform-specific components

---
+++ EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialFeediOSTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Sergey Kudryavtsev on 9/19/26.
+//
+
+import XCTest
+@testable import EssentialFeediOS
+
+final class EssentialFeediOSTests: XCTestCase {
+
+
+}
